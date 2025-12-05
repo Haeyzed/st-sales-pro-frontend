04217be3b85e1e2f66c9ab715de3ec5a95c27245
Haeyzed <muibi.azeezabolade@gmail.com>
2025-12-06 00:26:43 +0100

    update

--- components/units/data/schema.ts
+++ components/units/data/schema.ts
@@ -4,20 +4,17 @@ const unitSchemaBase = z.object({
   id: z.number(),
   unit_code: z.string(),
   unit_name: z.string(),
-  base_unit: z.number().nullable(),
-  operator: z.enum(["*", "/"]).nullable(),
-  operation_value: z.number().nullable(),
-  is_active: z.boolean().nullable(),
-  created_at: z.string(),
-  updated_at: z.string(),
+
+  base_unit: z.number().nullable().optional(),
+  operator: z.enum(["*", "/"]).nullable().optional(),
+  operation_value: z.number().nullable().optional(),
+  is_active: z.boolean().nullable().optional(),
+
+  created_at: z.string().nullable().optional(),
+  updated_at: z.string().nullable().optional(),
 })
 
-export const unitSchema: z.ZodType<
-  z.infer<typeof unitSchemaBase> & {
-    base_unit_relation?: z.infer<typeof unitSchemaBase> | null
-    derived_units?: z.infer<typeof unitSchemaBase>[]
-  }
-> = unitSchemaBase.extend({
+export const unitSchema: z.ZodType<any> = unitSchemaBase.extend({
   base_unit_relation: z.lazy(() => unitSchema.nullable()).optional(),
   derived_units: z.array(z.lazy(() => unitSchema)).optional(),
 })
